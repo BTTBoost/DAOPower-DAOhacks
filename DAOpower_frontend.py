0c6e0921322be3f6908bd7e28462ce8fc5b42880
jaimindp <jaiminpa@usc.edu>
2022-04-10 16:28:21 +0100

    more stats

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -9,16 +9,15 @@ df['USD'] = df['quote']
 summation = df.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
 num_members = len(df.address.unique())
 
-st.title('DAOpower')
+st.title('DAO Power')
+
+dao = st.selectbox('Pick DAO',['Friends With Benefits DAO', 'Other DAO'])
+
 
 st.header('DAO Member Stats')
 st.markdown('Number of DAO members: %d' % num_members)
 st.markdown('Total DAO Power: ${:,.2f}'.format(sum(summation)))
 st.markdown('DAO Power per Capita: ${:,.2f}'.format(sum(summation) / num_members))
-
-
-st.subheader('Holdings of DAO Members')
-dao = st.selectbox('Pick DAO',['Friends With Benefits DAO', 'Other DAO'])
 
 if dao == 'Friends With Benefits DAO':
 
