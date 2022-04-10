9635ea6ab9a4120e335b13d15ead697155b09997
jaimindp <jaiminpa@usc.edu>
2022-04-10 17:13:40 +0100

    added description

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -7,14 +7,16 @@ import requests
 df = pd.read_csv('dao_power.csv')
 
 url = 'https://api.covalenthq.com/v1/pricing/tickers/'
-params = {'page-size':'1000','key':'ckey_9cda6824fa45468f808ca8c0c0e'}
+params = {'page-size':'200','key':'ckey_9cda6824fa45468f808ca8c0c0e'}
 r = requests.get(url, params)
 tickers_json = r.json()
 
 include_tickers = set()
 
 for i in tickers_json['data']['items']:
-    include_tickers.add(i['contract_ticker_symbol'])
+    if i['contract_ticker_symbol'] not in ['META','DEGEN','ALPHA','ENS','LOOKS','GNO']:
+        include_tickers.add(i['contract_ticker_symbol'])
+    
     
 df = df[df['contract_ticker_symbol'].isin(include_tickers)]
 
