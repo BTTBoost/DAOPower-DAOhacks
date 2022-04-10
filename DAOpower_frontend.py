f1b9ece41966feb6045ebbfed8da317c14243e8b
jaimindp <jaiminpa@usc.edu>
2022-04-10 17:10:54 +0100

    added description

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -2,13 +2,27 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
 
 df = pd.read_csv('dao_power.csv')
+
+url = 'https://api.covalenthq.com/v1/pricing/tickers/'
+params = {'page-size':'1000','key':'ckey_9cda6824fa45468f808ca8c0c0e'}
+r = requests.get(url, params)
+tickers_json = r.json()
+
+include_tickers = set()
+
+for i in tickers_json['data']['items']:
+    include_tickers.add(i['contract_ticker_symbol'])
+    
+df = df[df['contract_ticker_symbol'].isin(include_tickers)]
+
 
 df['USD'] = df['quote']
 summation = df.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
 num_members = len(df.address.unique())
-
+          
 st.title('DAO Power')
 
 st.subheader('DAO Power is the total wealth held by the specific DAO community')
@@ -23,7 +37,6 @@ if dao == 'Friends With Benefits DAO':
     st.markdown('Total DAO Power: ${:,.2f}'.format(sum(summation)))
     st.markdown('DAO Power per Capita: ${:,.2f}'.format(sum(summation) / num_members))
 
-
     st.header('Top tokens holdings of %d DAO members' % len(df.address.unique()))
 
     st.bar_chart(summation)
@@ -32,7 +45,7 @@ if dao == 'Friends With Benefits DAO':
 
     tokens = list(summation.index)
 
-    selected = st.multiselect('Select tokens to view:', tokens, default=['ETH'])
+    selected = st.multiselect('Select tokens to view:', tokens, default=['WETH','USDT'])
 
     if tokens != selected:
         st.bar_chart(summation[summation.index.isin(selected)])
