2bbcc71c40f549614c8aaca79b695f53be71d041
jaimindp <jaiminpa@usc.edu>
2022-04-10 16:26:46 +0100

    more stats

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -4,27 +4,38 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('dao_power_small.csv')
+
 df['USD'] = df['quote']
 summation = df.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
-
+num_members = len(df.address.unique())
 
 st.title('DAOpower')
-st.header('Holdings of the DAO Members')
-st.selectbox('Pick DAO',['Friends With Benefits DAO'])
 
-st.header('Top tokens holdings of DAO members')
-
-st.bar_chart(summation)
-st.dataframe(summation)
+st.header('DAO Member Stats')
+st.markdown('Number of DAO members: %d' % num_members)
+st.markdown('Total DAO Power: ${:,.2f}'.format(sum(summation)))
+st.markdown('DAO Power per Capita: ${:,.2f}'.format(sum(summation) / num_members))
 
 
-tokens = list(summation.index)
+st.subheader('Holdings of DAO Members')
+dao = st.selectbox('Pick DAO',['Friends With Benefits DAO', 'Other DAO'])
 
-selected = st.multiselect('Select tokens to view:', tokens, default=['ETH','LINK'])
+if dao == 'Friends With Benefits DAO':
 
-if tokens != selected:
-    st.bar_chart(summation[summation.index.isin(selected)])
+    st.header('Top tokens holdings of %d DAO members' % len(df.address.unique()))
 
-    
+    st.bar_chart(summation)
+    st.dataframe(summation)
+
+
+    tokens = list(summation.index)
+
+    selected = st.multiselect('Select tokens to view:', tokens, default=['ETH','LINK'])
+
+    if tokens != selected:
+        st.bar_chart(summation[summation.index.isin(selected)])
+
+else:
+    st.subheader('More DAOs coming soon')
 
 
