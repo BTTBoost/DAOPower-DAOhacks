a9a24c7934630fc47b24bc3f5e5a37c4c5d09697
jaimindp <jaiminpa@usc.edu>
2022-04-10 16:11:10 +0100

    selecting tokens

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -1,20 +1,30 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('dao_power_small.csv')
+df['USD'] = df['quote']
+summation = df.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
 
 
 st.title('DAOpower')
-st.subheader('Holdings of the DAO Members')
-
+st.header('Holdings of the DAO Members')
 st.selectbox('Pick DAO',['Friends With Benefits DAO'])
 
-# st.dataframe(df)
+st.header('Top tokens holdings of DAO members')
 
-
-summation = df.groupby(['contract_ticker_symbol'])['quote'].sum().sort_values(ascending=False).head(30)
-
-st.dataframe(summation)
 st.bar_chart(summation)
+st.dataframe(summation)
+
+
+tokens = list(summation.index)
+
+selected = st.multiselect('Select tokens to view:', tokens, default=['ETH','LINK'])
+
+if tokens != selected:
+    st.bar_chart(summation[summation.index.isin(selected)])
+
+    
+
 
