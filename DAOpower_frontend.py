39bdaa52e2cfa25d774b279093cc04e54c8ca091
jaimindp <jaiminpa@usc.edu>
2022-04-10 22:05:57 +0100

    added description

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -5,6 +5,7 @@ import numpy as np
 import requests
 
 df = pd.read_csv('dao_power.csv')
+df2 = pd.read_csv('dao_power2.csv')
 
 url = 'https://api.covalenthq.com/v1/pricing/tickers/'
 params = {'page-size':'200','key':'ckey_9cda6824fa45468f808ca8c0c0e'}
@@ -19,38 +20,64 @@ for i in tickers_json['data']['items']:
     
     
 df = df[df['contract_ticker_symbol'].isin(include_tickers)]
+df2 = df2[df2['contract_ticker_symbol'].isin(include_tickers)]
 
 
 df['USD'] = df['quote']
+df2['USD'] = df2['quote']
 summation = df.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
+summation2 = df2.groupby(['contract_ticker_symbol'])['USD'].sum().sort_values(ascending=False).head(30)
 num_members = len(df.address.unique())
+num_members2 = len(df2.address.unique())
           
 st.title('DAO Power')
 
 st.subheader('DAO Power is the total wealth held by the specific DAO community')
 
-dao = st.selectbox('Pick DAO',['Friends With Benefits DAO', 'Other DAO'])
-
+dao = st.selectbox('Pick DAO',['Friends With Benefits DAO', 'ClearDAO','Other DAO'])
 
 if dao == 'Friends With Benefits DAO':
     st.header('DAO Member Stats')
 
-    st.markdown('Number of DAO members: %d' % num_members)
+    st.markdown('Number of DAO members: {:,d}'.format(num_members))
     st.markdown('Total DAO Power: ${:,.2f}'.format(sum(summation)))
     st.markdown('DAO Power per Capita: ${:,.2f}'.format(sum(summation) / num_members))
 
-    st.header('Top tokens holdings of %d DAO members' % len(df.address.unique()))
+    st.header('Top tokens holdings of {:,d} DAO members'.format(num_members))
 
     st.bar_chart(summation)
     st.dataframe(summation)
-
+    st.caption('Top Token Holding')
 
     tokens = list(summation.index)
 
-    selected = st.multiselect('Select tokens to view:', tokens, default=['WETH','USDT'])
+    selected = st.multiselect('Select tokens to view:', tokens, default=['USDT','USDC'])
 
     if tokens != selected:
         st.bar_chart(summation[summation.index.isin(selected)])
 
+
+elif dao == 'ClearDAO':
+    
+    st.header('DAO Member Stats')
+
+    st.markdown('Number of DAO members: {:,d}'.format(num_members2))
+    st.markdown('Total DAO Power: ${:,.2f}'.format(sum(summation2)))
+    st.markdown('DAO Power per Capita: ${:,.2f}'.format(sum(summation2) / num_members2))
+
+    st.header('Top tokens holdings of {:,d} DAO members'.format(num_members2))
+
+    st.bar_chart(summation2)
+    st.dataframe(summation2)
+    st.caption('Top Token Holding')
+
+
+    tokens2 = list(summation2.index)
+
+    selected = st.multiselect('Select tokens to view:', tokens2, default=['USDT','USDC'])
+
+    if tokens2 != selected:
+        st.bar_chart(summation2[summation2.index.isin(selected)])
+    
 else:
     st.subheader('More DAOs coming soon')
