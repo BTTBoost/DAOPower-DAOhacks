2e1acaa2b682ca7dc8dfa8c1aad9f43f91fc02b9
BTT Boost <85220952+BTTBoost@users.noreply.github.com>
2022-05-31 02:08:31 +0900

    Update DAOpower_frontend.py

--- DAOpower_frontend.py
+++ DAOpower_frontend.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import requests
 
-df = pd.read_csv('dao_power.csv')
+df = pd.read_csv('dao_power_small.csv')
 df2 = pd.read_csv('dao_power2.csv')
 
 url = 'https://api.covalenthq.com/v1/pricing/tickers/'
